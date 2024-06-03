4fc1e83a523ad69a39152081e94b5ef9b479fb58
Tomi Kartaslammi <tkartasl@c1r2p6.hive.fi>
2024-06-03 15:06:52 +0300

    code is still a mess but raycasting should work now

--- srcs/raycast.c
+++ srcs/raycast.c
@@ -6,7 +6,7 @@
 /*   By: tkartasl <tkartasl@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/05/28 09:16:02 by tkartasl          #+#    #+#             */
-/*   Updated: 2024/05/31 16:06:28 by tkartasl         ###   ########.fr       */
+/*   Updated: 2024/06/03 15:05:20 by tkartasl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -24,17 +24,11 @@
 #define NORTH PI / 2
 #define WEST PI
 #define SOUTH 3 * PI / 2
-
-typedef struct s_mlx
+#define DEGREE 0.01745329251
+typedef struct s_rayinfo
 {
 	mlx_t* mlx;
 	mlx_image_t* black;
- 	mlx_image_t* screen;
-	mlx_image_t* player;
-}			t_mlx_stuff;
-
-typedef struct s_rayinfo
-{
 	mlx_image_t *player;
 	mlx_image_t *screen;
 	double	angle;
@@ -71,7 +65,7 @@ int	compare(int a, int b)
 	return (ret);
 }
 
-void	draw_line(int x_end, int y_end, int x_start, int y_start, mlx_image_t *screen)
+void	draw_line(int x_end, int y_end, int x_start, int y_start, mlx_image_t * screen)
 {
 	int dx = abs(x_end - x_start);
 	int	sx = compare(x_start, x_end);
@@ -99,15 +93,153 @@ void	draw_line(int x_end, int y_end, int x_start, int y_start, mlx_image_t *scre
 			y_start += sy;
 		}
 	}
-	if (mlx_image_to_window(mlx, screen, 0, 0) < 0)
-        error();
+}
+
+double	check_vertical_hit(double px, double py, t_rayinfo *info, double *vx, double *vy)
+{
+	int arr[5][5] = {{1, 1, 1, 1, 1}, {1, 0, 0, 0, 1}, {1, 0, 1, 0, 1}, {1, 0, 0, 0, 1}, {1, 1, 1, 1, 1}};
+	double	ray_y = 0;
+	double	ray_x = 0;
+	int		viewdist;
+	double	step_x = 0;
+	double	step_y = 0;
+	int		map_x = 0;
+	int		map_y = 0;
+	double	ntan = 0;
+	double	dist_v = 100000;
+	int		height = 5;
+	int		width = 5;
+
+	viewdist = 0;
+	ntan = -tan(info->rayangle);
+	if (info->rayangle > PI2 && info->rayangle < PI3)
+	{
+		ray_x = (((int)px >> 6) << 6) - 0.0001;
+		ray_y = (px - ray_x) * ntan + py;
+		step_x = -64;
+		step_y = -step_x * ntan;
+	}
+	if (info->rayangle < PI2 || info->rayangle > PI3)
+	{
+		ray_x = (((int)px >> 6) << 6) + 64;
+		ray_y = (px - ray_x) * ntan + py;
+		step_x = 64;
+		step_y = -step_x * ntan;
+	}
+	if (info->rayangle == 0 || info->rayangle == PI)
+	{
+		ray_x = px;
+		ray_y = py;
+		viewdist = 8;
+	}
+	while (viewdist < 8)
+	{
+		map_x = (int)ray_x >> 6;				
+		map_y = (int)ray_y >> 6;
+		if ((map_x >= 0 && map_x < width) && (map_y >= 0 && map_y < height) && arr[map_y][map_x] == 1)
+		{
+			//printf("mapv_x = %d\nmapv_y = %d\n", mapv_x, mapv_y);
+			*vx = ray_x;
+			*vy = ray_y;
+			dist_v = ray_length(px, py, ray_x, ray_y);
+			viewdist = 8;
+		}
+		else
+		{
+			ray_x += step_x;
+			ray_y += step_y;
+			viewdist += 1;
+			}
+		}
+		return (dist_v);
+}
+
+double	check_horizontal_hit(double px, double py, t_rayinfo *info, double *hx, double *hy)
+{
+	int arr[5][5] = {{1, 1, 1, 1, 1}, {1, 0, 0, 0, 1}, {1, 0, 1, 0, 1}, {1, 0, 0, 0, 1}, {1, 1, 1, 1, 1}};
+	double	ray_y = 0;
+	double	ray_x = 0;
+	int		viewdist;
+	double	step_x = 0;
+	double	step_y = 0;
+	int		map_x = 0;
+	int		map_y = 0;
+	double	atan = 0;
+	double	dist_h = 100000;
+	int		height = 5;
+	int		width = 5;
+
+	viewdist = 0;
+	atan = -1 / tan(info->rayangle);
+	if (info->rayangle > PI)
+	{
+		ray_y = (((int)py >> 6) << 6) - 0.0001;
+		ray_x = (py - ray_y) * atan + px;
+		step_y = -64;
+		step_x = -step_y * atan;
+		}
+		if (info->rayangle < PI)
+		{
+			ray_y = (((int)py >> 6) << 6) + 64;
+			ray_x = (py - ray_y) * atan + px;
+			step_y = 64;
+			step_x = -step_y * atan;
+		}
+		if (info->rayangle == PI || info->rayangle == 0)
+		{
+			ray_x = px;
+			ray_y = py;
+			viewdist = 8;
+		}
+		while (viewdist < 8)
+		{
+			map_x = (int)ray_x >> 6;				
+			map_y = (int)ray_y >> 6;
+			if ((map_x >= 0 && map_x < width) && (map_y >= 0 && map_y < height) && arr[map_y][map_x] == 1)
+			{
+				//printf("maph_x = %d\nmaph_y = %d\n", maph_x, maph_y);
+				*hx = ray_x;
+				*hy = ray_y;
+				dist_h = ray_length((double)info->player->instances[0].x, (double)info->player->instances[0].y, ray_x, ray_y);
+				viewdist = 8;
+			}
+			else
+			{
+				ray_x += step_x;
+				ray_y += step_y;
+				viewdist += 1;
+			}
+		}
+		return (dist_h);
+}
+
+void	cast_rays(t_rayinfo *info)
+{
+	int arr[5][5] = {{1, 1, 1, 1, 1}, {1, 0, 0, 0, 1}, {1, 0, 1, 0, 1}, {1, 0, 0, 0, 1}, {1, 1, 1, 1, 1}};
+	double	dist_h = 0;
+	double	dist_v = 0;
+	int		i = 0;
+	double	px = info->player->instances[0].x;
+	double 	py = info->player->instances[0].y;
+	double	hx = 0;
+	double 	vx = 0;
+	double	hy = 0;
+	double 	vy = 0;
+	int		j = 0;
+
+	mlx_image_t* screen = mlx_new_image(info->mlx, 320, 320);
+	if (!screen)
+		error();
+	memset(screen->pixels, 255, 320 * 320 * sizeof(int32_t));
+	if (mlx_image_to_window(info->mlx, screen, 0, 0) < 0)
+    	error();
 	while (j < 5)
 	{
 		while (i < 5)
 		{
 			if (arr[j][i] == 1)
 			{
-				if (mlx_image_to_window(mlx, black, i * 64, j * 64) < 0)
+				if (mlx_image_to_window(info->mlx, info->black, i * 64, j * 64) < 0)
         			error();
 			}
 			i++;
@@ -115,123 +247,27 @@ void	draw_line(int x_end, int y_end, int x_start, int y_start, mlx_image_t *scre
 		i = 0;
 		j++;
 	}
-	if (mlx_image_to_window(mlx, rayinfo.player, 96, 224) < 0)
+	if (mlx_image_to_window(info->mlx, info->player, info->player->instances[0].x, info->player->instances[0].y) < 0)
 		error();
-}
-
-void	cast_rays(t_rayinfo *info)
-{
-	int arr[5][5] = {{1, 1, 1, 1, 1}, {1, 0, 0, 0, 1}, {1, 0, 0, 0, 1}, {1, 0, 0, 0, 1}, {1, 1, 1, 1, 1}};
-	double	rayh_y = 0;
-	double	rayh_x = 0;
-	double	rayv_y = 0;
-	double	rayv_x = 0;
-	//double	plane_x = 0;
-	//double	plane_y = 0.66;
-	//double	time = 0;
-	//double	oldtime = 0;
-	int		viewdist;
-	//double	ray_count;
-	double	step_x = 0;
-	double	step_y = 0;
-	int		maph_x = 0;
-	int		maph_y = 0;
-	int		mapv_x = 0;
-	int		mapv_y = 0;
-	double	atan = 0;
-	double	ntan = 0;
-	double	dist_h = 100000;
-	double	dist_v = 100000;
-	int		i = 0;
-	int		height = 5;
-	int		width = 5;
-	
-	while (i < 1)
+	info->rayangle = info->angle - DEGREE * FOV / 2;
+	if (info->rayangle < 0)
+		info->rayangle += 2 * PI;
+	if (info->rayangle > 2 * PI)
+		info->rayangle -= 2 * PI;
+	while (i < FOV)
 	{
-		viewdist = 0;
-		atan = -1/tan(rayangle);
-		if (rayangle > WEST)
-		{
-			rayh_y = ((info->player->instances[0].y >> 6) << 6) - 0.0001;
-			rayh_x = (info->player->instances[0].y - rayh_y) * atan + info->player->instances[0].x;
-			step_y = -64;
-			step_x = -step_y * atan;
-		}
-		if (rayangle < WEST)
-		{
-			rayh_y = ((info->player->instances[0].y >> 6) << 6) + 64;
-			rayh_x = (info->player->instances[0].y - rayh_y) * atan + info->player->instances[0].x;
-			step_y = 64;
-			step_x = -step_y * atan;
-		}
-		if (rayangle == 0 || rayangle == PI)
-		{
-			rayh_x = info->player->instances[0].x;
-			rayh_y = info->player->instances[0].y;
-			viewdist = 8;
-		}
-		while (viewdist < 8)
-		{
-			maph_x = (int)rayh_x >> 6;				
-			maph_y = (int)rayh_y >> 6;
-			printf("maph_x = %d\nmaph_y = %d\n", maph_x, maph_y);
-			if ((maph_x >= 0 && maph_x < width) && (maph_y >= 0 && maph_y < height) && arr[maph_y][maph_x] == 1)
-			{
-				dist_h = ray_length(info->player->instances[0].x, info->player->instances[0].y, rayh_x, rayh_y);
-				viewdist = 8;
-			}
-			else
-			{
-				rayh_x += step_x;
-				rayh_y += step_y;
-				viewdist += 1;
-			}
-		}
-		viewdist = 0;
-		ntan = -tan(rayangle);
-		if (rayangle > NORTH && rayangle < SOUTH)
-		{
-			rayv_x = ((info->player->instances[0].x >> 6) << 6) - 0.0001;
-			rayv_y = (info->player->instances[0].x - rayv_x) * ntan + info->player->instances[0].y;
-			step_x = -64;
-			step_y = -step_x * ntan;
-		}
-		if (rayangle < NORTH || rayangle > SOUTH)
-		{
-			rayv_x = ((info->player->instances[0].x >> 6) << 6) + 64;
-			rayv_y = (info->player->instances[0].x - rayv_x) * ntan + info->player->instances[0].y;
-			step_x = 64;
-			step_y = -step_x * ntan;
-		}
-		if (rayangle == 0 || rayangle == PI)
-		{
-			rayv_x = info->player->instances[0].x;
-			rayv_y = info->player->instances[0].y;
-			viewdist = 8;
-		}
-		while (viewdist < 8)
-		{
-			mapv_x = (int)rayv_x >> 6;				
-			mapv_y = (int)rayv_y >> 6;
-			printf("mapv_x = %d\nmapv_y = %d\n", mapv_x, mapv_y);
-			if ((mapv_x >= 0 && mapv_x < width) && (mapv_y >= 0 && mapv_y < height) && arr[mapv_y][mapv_x] == 1)
-			{
-				dist_v = ray_length(info->player->instances[0].x, info->player->instances[0].y, rayv_x, rayv_y);
-				viewdist = 8;
-			}
-			else
-			{
-				rayv_x += step_x;
-				rayv_y += step_y;
-				viewdist += 1;
-			}
-		}
-		printf("dist_h = %f\ndist_v = %f\n", dist_h, dist_v);
-		if (dist_h >= dist_v)
-			draw_line(info->player->instances[0].x, info->player->instances[0].y, (int)rayv_x, (int)rayv_y, info->screen);
+		dist_h = check_horizontal_hit(px, py, info, &hx, &hy);
+		dist_v = check_vertical_hit(px, py, info, &vx, &vy);
+		printf("dist_h %f\ndist_v %f\n", dist_h, dist_v);
+		if (dist_v >= dist_h)
+			draw_line(px, py, hx, hy, screen);
 		else
-			draw_line(info->player->instances[0].x, info->player->instances[0].y, (int)rayh_x, (int)rayh_y, info->screen);
-		//info->ray_angle
+			draw_line(px, py, vx, vy, screen);
+		info->rayangle += DEGREE;
+		if (info->rayangle < 0)
+			info->rayangle += 2 * PI;
+		if (info->rayangle > 2 * PI)
+			info->rayangle -= 2 * PI;
 		i++;
 	}
 }
@@ -246,43 +282,45 @@ void	key_hook_move(mlx_key_data_t keydata, void* param)
 		info->angle += 0.2;
 		if (info->angle > 2 * PI)
 			info->angle -= 2 * PI;
+		//info->rayangle = info->angle;
 		info->playerdir_x = cos(info->angle) * 10;
 		info->playerdir_y = sin(info->angle) * 10;
-		//cast_rays(info);
+	//	cast_rays(info);
 	}
 	if (keydata.key == MLX_KEY_LEFT && (keydata.action == MLX_REPEAT || keydata.action == MLX_PRESS))
 	{
 		info->angle -= 0.2;
 		if (info->angle < 0)
 			info->angle += 2 * PI;
+		//info->rayangle = info->angle;
 		info->playerdir_x = cos(info->angle) * 10;
 		info->playerdir_y = sin(info->angle) * 10;
-		//cast_rays(info);
+	//	cast_rays(info);
 	}
 	//printf("player dir x %f , player dir y %f\n" ,info->playerdir_x, info->playerdir_y);
 	if (keydata.key == MLX_KEY_D && (keydata.action == MLX_REPEAT || keydata.action == MLX_PRESS))
 	{
-		info->player->instances[0].x -= (int)info->playerdir_x;
+		//info->player->instances[0].x -= (int)info->playerdir_x;
 		info->player->instances[0].x += (int)info->playerdir_y;
 		cast_rays(info);
 	}
 	//printf("player dir x %f , player dir y %f\n" ,info->playerdir_x, info->playerdir_y);
 	if (keydata.key == MLX_KEY_A && (keydata.action == MLX_REPEAT || keydata.action == MLX_PRESS))
 	{
-		info->player->instances[0].x += (int)info->playerdir_x;
+		//info->player->instances[0].x += (int)info->playerdir_x;
 		info->player->instances[0].x -= (int)info->playerdir_y;
 		cast_rays(info);
 	}
 	if (keydata.key == MLX_KEY_W && (keydata.action == MLX_REPEAT || keydata.action == MLX_PRESS))
 	{
-		info->player->instances[0].x -= (int)info->playerdir_x;
-		info->player->instances[0].y -= (int)info->playerdir_y;
+		info->player->instances[0].x += (int)info->playerdir_x;
+		info->player->instances[0].y += (int)info->playerdir_y;
 		cast_rays(info);
 	}
 	if (keydata.key == MLX_KEY_S && (keydata.action == MLX_REPEAT || keydata.action == MLX_PRESS))
 	{
-		info->player->instances[0].x += (int)info->playerdir_x;
-		info->player->instances[0].y += (int)info->playerdir_y;
+		info->player->instances[0].x -= (int)info->playerdir_x;
+		info->player->instances[0].y -= (int)info->playerdir_y;
 		cast_rays(info);
 	}
 	//printf("%d x: \n%d y: ", info->player->instances->x, info->player->instances->y);
@@ -293,37 +331,34 @@ void	key_hook_move(mlx_key_data_t keydata, void* param)
 int32_t	main(void)
 {
 	// Start mlx
-	int arr[5][5] = {{1, 1, 1, 1, 1}, {1, 0, 0, 0, 1}, {1, 0, 0, 0, 1}, {1, 0, 0, 0, 1}, {1, 1, 1, 1, 1}};
+	int arr[5][5] = {{1, 1, 1, 1, 1}, {1, 0, 0, 0, 1}, {1, 0, 1, 0, 1}, {1, 0, 0, 0, 1}, {1, 1, 1, 1, 1}};
 	int	i = 0;
 	int	j = 0;
 	t_rayinfo	rayinfo;
-	t_mlx_stuff	imgs;
 
 	mlx_t* mlx = mlx_init(320, 320, "Test", true);
 	if (!mlx)
         error();
 	memset(&rayinfo, 0, sizeof(t_rayinfo));
-	memset(&imgs, 0, sizeof(t_mlx_stuff));
-	imgs.mlx = mlx;
+	rayinfo.mlx = mlx;
 	mlx_image_t* black = mlx_new_image(mlx, 63, 63);
 	if (!black)
 		error();
-	rayinfo.player = mlx_new_image(mlx, 8, 8);
+	rayinfo.player = mlx_new_image(mlx, 4, 4);
 	if (!rayinfo.player)
 		error();
 	mlx_image_t* screen = mlx_new_image(mlx, 320, 320);
 	if (!screen)
 		error();
 	rayinfo.screen = screen;
-	imgs.black = black;
-	imgs.screen = screen;
-	imgs.player = rayinfo.player;
-	rayinfo.angle = 120 * PI / 180;
+	rayinfo.black = black;
+	rayinfo.player = rayinfo.player;
+	rayinfo.angle = 290 * PI / 180;
 	rayinfo.rayangle = rayinfo.angle;
 	rayinfo.playerdir_x = cos(rayinfo.angle) * 10;
 	rayinfo.playerdir_y = sin(rayinfo.angle) * 10;
 	memset(black->pixels, 100, 64 * 64 * sizeof(int32_t));
-	memset(rayinfo.player->pixels, 127, 8 * 8 * sizeof(int32_t));
+	memset(rayinfo.player->pixels, 127, 4 * 4 * sizeof(int32_t));
 	memset(screen->pixels, 255, 320 * 320 * sizeof(int32_t));
 	if (mlx_image_to_window(mlx, screen, 0, 0) < 0)
         error();
