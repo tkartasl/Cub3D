7f186df8d0ffe1151b639953628e1757a5a84361
Ahmed <adusman@lm1-500-25454.local>
2024-06-17 14:13:56 +0300

    added error function

--- srcs/free/free_mem.c
+++ srcs/free/free_mem.c
@@ -12,10 +12,21 @@
 
 # include "../../includes/cub3D.h"
 
+
 void	file_error(char *path, int use_errno)
 {
+	if (use_errno == YES)
+	{
+		ft_putstr_fd("cub3d: ", 2);
+		ft_putstr_fd(path, 2);
+		ft_putstr_fd(": ", 2);
+		ft_putendl_fd(strerror(errno), 2);
+		ft_putstr_fd("", 2);
+	}
+	ft_putstr_fd("cub3d: ", 2);
 	ft_putstr_fd(path, 2);
-	ft_putendl_fd("file path invalid", 2);
+	ft_putendl_fd(": is not valid map file", 2);
+	exit(EXIT_FAILURE);
 }
 
 static	void	free_lines(t_cub *cub, int lines)
--- srcs/main/main.c
+++ srcs/main/main.c
@@ -34,4 +34,3 @@ int	main(int argc, char **argv)
 	game_usage();
 	return (SUCCESS);
 }
-
