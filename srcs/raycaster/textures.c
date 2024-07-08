f9e8b8b81aeb92131e094e89c7b9a9aaac10b175
Tomi Kartaslammi <tkartasl@c1r4p9.hive.fi>
2024-07-08 13:40:49 +0300

    added args

--- srcs/raycaster/textures.c
+++ srcs/raycaster/textures.c
@@ -6,7 +6,7 @@
 /*   By: tkartasl <tkartasl@student.hive.fi>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/07/08 11:01:45 by tkartasl          #+#    #+#             */
-/*   Updated: 2024/07/08 13:27:03 by tkartasl         ###   ########.fr       */
+/*   Updated: 2024/07/08 13:35:58 by tkartasl         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -20,14 +20,14 @@ static void	load_north_south_textures(t_data *data, int dir)
 	{
 		tex = mlx_load_png("textures/arrow_south_small.png");
 		if (tex == NULL)
-			freedata_exit(data, EXIT_FAILURE, YES);
+			freedata_exit(data, EXIT_FAILURE, YES, NA);
 		data->player = mlx_texture_to_image(data->mlx, tex);
 	}
 	else
 	{
 		tex = mlx_load_png("textures/arrow_north_small.png");
 		if (tex == NULL)
-			freedata_exit(data, EXIT_FAILURE, YES);
+			freedata_exit(data, EXIT_FAILURE, YES, NA);
 		data->player = mlx_texture_to_image(data->mlx, tex);
 	}
 	mlx_delete_texture(tex);
@@ -41,14 +41,14 @@ static void	load_east_west_textures(t_data *data, int dir)
 	{
 		tex = mlx_load_png("textures/arrow_west_small.png");
 		if (tex == NULL)
-			freedata_exit(data, EXIT_FAILURE, YES);
+			freedata_exit(data, EXIT_FAILURE, YES, NA);
 		data->player = mlx_texture_to_image(data->mlx, tex);
 	}
 	else
 	{
 		tex = mlx_load_png("textures/arrow_east_small.png");
 		if (tex == NULL)
-			freedata_exit(data, EXIT_FAILURE, YES);
+			freedata_exit(data, EXIT_FAILURE, YES, NA);
 		data->player = mlx_texture_to_image(data->mlx, tex);
 	}
 	mlx_delete_texture(tex);
