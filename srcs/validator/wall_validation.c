dae9694acf0dbfc86252198fb0e2dc5a4a83dd33
Usman Ahmed <uahmed@c1r1p9.hive.fi>
2024-07-12 16:56:58 +0300

    added null comarisons for player void space

--- srcs/validator/wall_validation.c
+++ srcs/validator/wall_validation.c
@@ -24,11 +24,37 @@ int	get_line_end(char *line)
 	return (i);
 }
 
+
+static void    check_middle_lines(char **map, int x, int y, t_parser *parser)
+{
+       int     len_top;
+       int     len_bot;
+
+       len_top = get_line_end(map[y - 1]);
+       len_bot = get_line_end(map[y + 1]);
+       if (len_bot <= len_top)
+       {
+               if (x > len_bot)
+                       x = len_bot + 1;
+       }
+       else
+       {
+               if (x > len_top)
+                       x = len_top + 1;
+       }
+       while (map[y][x] != 0)
+       {
+               if (map[y][x] == '0')
+                       free_vecs(parser, YES, MAPHOLE, map);
+               x++;
+       }
+}
+
 int	check_surroundings(char **map, t_data *data, int x, int y)
 {
 	if (y + 1 < data->map_height)
 	{
-		if (ft_isspace(map[y + 1][x]) == 1)
+		if (ft_isspace(map[y - 1][x]) == 1)
 			return (1);		
 	}
 	if (y - 1 >= 0)
@@ -48,6 +74,10 @@ void	check_mapholes(char **map, t_parser *parser, t_data *data)
 	y = 0;
 	while (map[y] != 0)
 	{
+		x = get_line_end(map[y]);
+		if (y > 0 && y < data->map_height - 1)
+			check_middle_lines(map, x, y, parser);
+		x = 0;
 		while (map[y][x] != 0)
 		{
 			if (map[y][x] == '0')
@@ -55,7 +85,6 @@ void	check_mapholes(char **map, t_parser *parser, t_data *data)
 					free_vecs(parser, YES, MAPHOLE, map);
 			x++;
 		}
-		x = 0;
 		y++;
 	}	
 }
